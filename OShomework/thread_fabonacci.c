983166b0d0b6b7afdc66578a4c9cdc4a2ad7e968
qwhying <woshiqiwanga@163.com>
2019-04-14 20:03:34 +0800

    线程实现斐波那契

--- OShomework/thread_fabonacci.c
+++ OShomework/thread_fabonacci.c
@@ -2,5 +2,52 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<pthread.h>
-void *thread_fibonacci(int num)
-int main(int argc,)
+#define MAX_SPACE 100
+/*int fibo[MAX_SPACE];
+    fibo[0]=(int)0;
+    fibo[1]=(int)1;*/
+ typedef struct Data{
+     int num; //斐波那契数列的项数
+     int fibo[MAX_SPACE];//最大容量，斐波那契数列
+}Data;
+void *thread_fibonacci(void* data)
+{
+    Data *tmp=(Data*)data;
+    int i;
+    int x0=0;
+    int x1=1;
+    int x2;
+    tmp->fibo[0]=0;
+    tmp->fibo[1]=1;
+    if(tmp->num<=2)return;
+    for( i=2;i<tmp->num;i++)
+    {
+        x2=x1+x0;
+        x0=x1;
+        x1=x2;
+        tmp->fibo[i]=x2;
+    }
+}
+int main(int argc,char* argv[])
+{
+    Data data;
+    data.num=atoi(argv[1]);
+    int res;
+    int i;
+    pthread_t a_thread;
+    res=pthread_create(&a_thread,NULL,thread_fibonacci,(void *)&data);
+    if(res!=0){perror("Thread creation failed");
+                          exit(EXIT_FAILURE);}
+    printf("Waiting for thread to finish..\n");
+    res=pthread_join(a_thread,NULL);
+    if(res!=0){perror("Thread  join failed");
+                          exit(EXIT_FAILURE);}
+    printf("The Fibonacci %d items are:\n",data.num);
+    for(int j=0;j<data.num;j++)
+    {
+        printf("%d ",data.fibo[j]);
+    }
+    printf("\n");
+    
+
+}
